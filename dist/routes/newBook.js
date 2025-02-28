a685721a2ea8c7aebce2adb73896dfc040edb36e
Sparven0 <gustafwingren@hotmail.se>
2025-02-28 17:27:31 +0100

    Add response message for new book creation

--- dist/routes/newBook.js
+++ dist/routes/newBook.js
@@ -1,26 +1,23 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { body, validationResult } from "express-validator";
 import { writeBook } from "../serverFunctions.js";
-
 export const newBookRouter = Router();
-
 const newBookValidation = [
     body('title').isString(),
     body('writer').isString(),
-]
-
-newBookRouter.post('/books/new', newBookValidation, async (req: Request, res: Response, next: NextFunction) => {
+];
+newBookRouter.post('/books/new', newBookValidation, async (req, res, next) => {
     const newBook = req.body;
     try {
         const errors = validationResult(req);
-        if(errors.array().length > 0){
-            res.status(400).json({errors: errors.array()});
+        if (errors.array().length > 0) {
+            res.status(400).json({ errors: errors.array() });
             throw new Error('Invalid input');
         }
         await writeBook(newBook);
-        res.json({message: 'Book added', newBook});
-    } catch (e) {
+        res.json({ message: 'Book added', newBook });
+    }
+    catch (e) {
         next(e);
     }
 });
-
